1ad08a0ad08c7652fdd4e1bcbe75fe74731f9f4d
BuildTools <unconfigured@null.spigotmc.org>
2020-09-26 17:36:54 +0300

    api

--- flask-app/app.py
+++ flask-app/app.py
@@ -33,18 +33,9 @@ news_data = [{
 }]
 
 
-<<<<<<< Updated upstream
-    data["title"] = "İstanbul Eczacı Odası, zatürre aşısı olması gereken risk gruplarını açıkladı"
-    data["url"] = "https://static.birgun.net/resim/haber-detay-resim/2020/09/24/istanbul-eczaci-odasi-zaturre-asisi-olmasi-gereken-risk-gruplarini-acikladi-784561-5.jpg"
-    data["alt"] = "news"
-    data["context"] = """İstanbul Eczacı Odası, zatürre aşısı (pnömokok) hakkında Hasta Bilgilendirme Broşürü hazırladı. Odadan yapılan açıklamaya göre broşürde, yeni tip koronavirüs (Covid-19) pandemisi nedeniyle kamuoyunun gündeminde yer alan zatürre aşısına ilişkin merak edilen konulara yer verildi. Broşürde aşılamanın, hastalıkların ve buna bağlı ölümlerin önlenmesi açısından en önemli ve en etkili toplum sağlığı hizmeti olduğu belirtilerek, aşı uygulamalarıyla hastalıkların ve hastalıklara bağlı ölüm ve sakatlıkların önlenmesi, hastalıkların hafif atlatılmasının sağlanması, toplumsal bağışıklık kazanılmasının hedeflendiği aktarıldı.
-        """
-    return data
-=======
 class News(Resource):
     def get(self):
         return news_data
 
 
 api.add_resource(News, "/news")
->>>>>>> Stashed changes
