ff275b9e0eb87e25e7d6ea2b2cc29bb791e5fa19
hacisonicos <36050418+hacisonicos@users.noreply.github.com>
2020-09-29 17:24:07 +0300

    navbar update

--- forum/src/components/navbar/navbar.js
+++ forum/src/components/navbar/navbar.js
@@ -2,30 +2,56 @@ import React from 'react';
 import './navbar.css';
 import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const MyButton = styled(Button)({
-    height: 22,
+    height: 25,
+    width: 140,
     background: "#00adb5",
     border: 0,
     borderRadius: 3,
     marginLeft: 5,
-    marginTop: 10,
-    color: "#222831"
+    color: "#222831",
+    justifySelf: "center",
+    alignSelf:"center"
 });
+
+const MySearch = styled(TextField)({
+    marginRight: 1,
+    marginLeft: 1,
+    width: '300px',
+    height: "40px",
+    borderRadius: 3,
+    background: "#00adb5",
+    '& label.Mui-focused': {
+        color: 'white',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: 'white',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: '#222831',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: 'white',
+        },
+      },
+    },
+);
+
 
 function Navbar() {
     return(
         <div className="Navbar">
             <header className="Navbar_header">
                 <h1 className="Navbar_text">News Feed</h1>
-                <form
-                >
-                    <input
-                    className="Navbar_search"
-                        type="text"
-                        placeholder="Search"
-                        />
-                </form>
+                <MySearch          
+                    label="Search"
+                    margin="dense"
+                    variant="outlined"
+                    type="search"
+                    size="small"/>
                 <MyButton variant="contained"> Sign In</MyButton>
                 <MyButton variant="contained"> Sign Up</MyButton>
             </header>
