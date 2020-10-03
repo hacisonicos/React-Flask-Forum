0331bb9583c09c1c2e3bb9a8bbba362237b0c7b2
hacisonicos <36050418+hacisonicos@users.noreply.github.com>
2020-10-03 16:40:36 +0300

    topic pages and their navigations

--- forum/src/App.js
+++ forum/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { useState, useEffect } from "react";
+//navigation
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+//material.ui
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
@@ -9,10 +16,17 @@ import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
 import "./App.css";
-import News from "./components/News/news";
-
+//pages
+import Home from "./topic/Home";
+import World from "./topic/World";
+import Nation from "./topic/Nation";
+import Business from "./topic/Business";
+import Technology from "./topic/Technology";
+import Entertainment from "./topic/Entertainment";
+import Science from "./topic/Science";
+import Sports from "./topic/Sports";
+import Health from "./topic/Health";
 
 const drawerWidth = 200;
 
@@ -37,20 +51,14 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  link:{
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  }
 }));
 
 export default function App() {
   const classes = useStyles();
-  const [newsData, setnewsData] = useState([]);
-
-  useEffect(() => {
-    fetch("/news")
-      .then((res) => res.json())
-      .then((data) => {
-        setnewsData(data);
-      });
-  }, []);
-
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -61,6 +69,7 @@ export default function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Router>
       <Drawer
         className={classes.drawer}
         variant="permanent"
@@ -71,34 +80,86 @@ export default function App() {
         <Toolbar />
         <div className={classes.drawerContainer}>
           <List>
-            {['World', 'Nation', 'Business', 'Technology', 'Entertainment', 'Science', 'Sports', 'Health'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemText primary={text} />
+            <Link to="/" className={classes.link}>
+              <ListItem button key="Home">
+                <ListItemText primary="Home"/>
               </ListItem>
-            ))}
+            </Link>
+            <Link to="/world" className={classes.link}>
+              <ListItem button key="World">
+                <ListItemText primary="World"/>
+              </ListItem>
+            </Link>
+            <Link to="/nation" className={classes.link}>
+              <ListItem button key="Nation">
+                <ListItemText primary="Nation"/>
+              </ListItem>
+            </Link>
+            <Link to="/business" className={classes.link}>
+              <ListItem button key="Business">
+                <ListItemText primary="Business"/>
+              </ListItem>
+            </Link>
+            <Link to="/technology" className={classes.link}>
+              <ListItem button key="Technology">
+                <ListItemText primary="Technology"/>
+              </ListItem>
+            </Link>
+            <Link to="/entertainment" className={classes.link}>
+              <ListItem button key="Entertainment">
+                <ListItemText primary="Entertainment"/>
+              </ListItem>
+            </Link>
+            <Link to="/science" className={classes.link}>
+              <ListItem button key="Science">
+                <ListItemText primary="Science"/>
+              </ListItem>
+            </Link>
+            <Link to="/sports" className={classes.link}>
+              <ListItem button key="Sports">
+                <ListItemText primary="Sports"/>
+              </ListItem>
+            </Link>
+            <Link to="/health" className={classes.link}>
+              <ListItem button key="Health">
+                <ListItemText primary="Health"/>
+              </ListItem>
+            </Link>
           </List>
         </div>
       </Drawer>
       <main className={classes.content}>
-        <Toolbar />
-        <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="md">
-      <Typography paragraph>
-          {newsData.map((news) => (
-                <News
-                  title={newsData.length > 0 && news.title}
-                  url={newsData.length > 0 && news.image_url}
-                  alt={newsData.length > 0 && news.description}
-                  context={newsData.length > 0 && news.description}
-                >
-                </News>
-            ))}
-        </Typography>
-      </Container>
-    </React.Fragment>
-
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/world">
+            <World/>
+          </Route>
+          <Route exact path="/nation">
+            <Nation/>
+          </Route>
+          <Route exact path="/business">
+            <Business/>
+          </Route>
+          <Route exact path="/technology">
+            <Technology/>
+          </Route>
+          <Route exact path="/entertainment">
+            <Entertainment/>
+          </Route>
+          <Route exact path="/science">
+            <Science/>
+          </Route>
+          <Route exact path="/sports">
+            <Sports/>
+          </Route>
+          <Route exact path="/health">
+            <Health/>
+          </Route>
+        </Switch>
       </main>
+      </Router>
     </div>
   );
 }
---
+++ forum/src/topic/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+//material.ui
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import News from "../components/News/news";
+
+export default function App() {
+  const [newsData, setnewsData] = useState([]);
+
+  useEffect(() => {
+    fetch("/news")
+      .then((res) => res.json())
+      .then((data) => {
+        setnewsData(data);
+      });
+  }, []);
+
+  return (
+    <div>
+        <Toolbar />
+        <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+      <Typography paragraph>
+          {newsData.map((news) => (
+                <News
+                  title={newsData.length > 0 && news.title}
+                  url={newsData.length > 0 && news.image_url}
+                  alt={newsData.length > 0 && news.description}
+                  context={newsData.length > 0 && news.description}
+                >
+                </News>
+            ))}
+        </Typography>
+      </Container>
+    </React.Fragment>
+    </div>
+  )};
