1d44225b99ebce68d370a20aedc6596169e16cc6
hacisonicos <36050418+hacisonicos@users.noreply.github.com>
2020-10-03 15:07:08 +0300

    changing app js

--- forum/src/App.js
+++ forum/src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React from 'react';
 import { useState, useEffect } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import "./App.css";
-import Navbar from "./components/navbar/navbar";
 import News from "./components/News/news";
 
-function App() {
+const drawerWidth = 200;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
   const [newsData, setnewsData] = useState([]);
 
   useEffect(() => {
@@ -19,25 +51,46 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Navbar />
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="md">
-          <Typography component="div">
-            {newsData.map((news) => (
-              <News
-                title={newsData.length > 0 && news.title}
-                url={newsData.length > 0 && news.image_url}
-                alt={newsData.length > 0 && news.description}
-                context={newsData.length > 0 && news.description}
-              ></News>
-            ))}
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            News Feed
           </Typography>
-        </Container>
-      </React.Fragment>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {['World', 'Nation', 'Business', 'Technology', 'Entertainment', 'Science', 'Sports', 'Health'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <Typography paragraph>
+          {newsData.map((news) => (
+                <News
+                  title={newsData.length > 0 && news.title}
+                  url={newsData.length > 0 && news.image_url}
+                  alt={newsData.length > 0 && news.description}
+                  context={newsData.length > 0 && news.description}
+                ></News>
+            ))}
+        </Typography>
+      </main>
     </div>
   );
 }
-
-export default App;
